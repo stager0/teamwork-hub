c4117203e9094876be43daf1ef66cecc9cf00cfd
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-03 23:22:14 +0200

    Added templates and logic for projects and tasks management
    - project-list: implemented project listing with search, navigation to project details and related tasks.
    - project-detail: added ability to view, update, and delete a project (for leaders).
    - task-list: displays all project tasks with their status and type; leaders can update and delete tasks.
    - task-detail: users can:
      - assign themselves to a task,
      - unassign themselves,
      - read task details,
      - view task type (feature, bug, etc.),
      - track task status (to do, in progress, code review, done),
      - update progress bar (0–100%) as they work.
    
    - user-tasks-list: allows users to view their current assigned tasks.
    - my-finished-tasks: displays tasks the user has completed.
    - users-list: shows a list of teammates with basic profile info.
    - our-successful-finished-projects: initial setup for displaying successfully completed projects.

--- teamwork_hub/urls.py
+++ teamwork_hub/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from workspace.views import CustomRegisterView, CustomLoginView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("workspace.urls", namespace="workspace")),
+    path("accounts/login/", CustomLoginView.as_view(), name="login"),
+    path("accounts/register/", CustomRegisterView.as_view(), name="register")
 ]
+
