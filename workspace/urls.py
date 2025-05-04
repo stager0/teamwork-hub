9162d0d4c9d2a39d51b7cfb6166c4dbcf51594c9
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-04 23:06:44 +0200

    feat: Define URL patterns for workspace app
    - Added URL routes for:
      - Index (home) view
      - User and worker management (list, detail, tasks, archive)
      - Project management (list, detail, create, update, delete, archive)
      - Task management (list, detail, create, update, delete, archive)
      - Task-specific actions: assign, unassign, progress update
    
    - Ensures clear routing to all implemented views for consistent navigation.

---
+++ workspace/urls.py
@@ -0,0 +1,40 @@
+from django.urls import path
+
+from workspace.views import index, ProjectsListView, ProjectsListCreateView, ProjectsListUpdateView, \
+    ProjectsListDeleteView, \
+    TasksListView, UserDetailView, TaskUpdateView, TaskDeleteView, TaskCreateView, TaskDetailView, UsersListView, \
+    assign_to_task, TaskExecutionChangeView, ProjectsDetailView, TaskModalUpdateView, ProjectsTaskDetailView, \
+    UserTasksListView, ProjectsArchivedListView, TaskUserArchiveView, task_in_archive, TaskReviewListView
+
+app_name = "workspace"
+
+urlpatterns = [
+    path("", index, name="index"),
+
+    path("user_detail/<int:pk>/", UserDetailView.as_view(), name="user-detail"),
+    path("users-list/", UsersListView.as_view(), name="workers-list"),
+    path("user-tasks-list/<int:pk>/", UserTasksListView.as_view(), name="user-tasks-list"),
+
+    path("projects-list/", ProjectsListView.as_view(), name="projects-list"),
+    path("projects-list/<int:pk>/detail/", ProjectsTaskDetailView.as_view(), name="projects-list-detail"),
+    path("projects-list/<int:pk>/update/", ProjectsListUpdateView.as_view(), name="projects-list-update"),
+    path("projects-list/create/", ProjectsListCreateView.as_view(), name="projects-list-create"),
+    path("projects-list/<int:pk>/delete/", ProjectsListDeleteView.as_view(), name="projects-list-delete"),
+    path("projects-archieve/", ProjectsArchivedListView.as_view(), name="projects-archive"),
+
+    path("projects-list/<int:project_id>/tasks/", TasksListView.as_view(), name="project-tasks-list"),
+    path("projects-list/<int:pk>/", ProjectsDetailView.as_view(), name="project-detail"),
+
+    path("task-detail/<int:pk>/", TaskDetailView.as_view(), name="task-detail"),
+    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("task/<int:pk>/modal-update/", TaskModalUpdateView.as_view(), name="task-modal-update"),
+    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
+    path("project/<int:project_id>/task/<int:pk>/", TaskDetailView.as_view(), name="project-task-detail"),
+    path("task/<int:pk>/assign/", assign_to_task, name="assign-to-task"),
+    path("task/<int:pk>/change-task-execution/", TaskExecutionChangeView.as_view(), name="change-task-execution"),
+    path("task/<int:pk>/archive-tasks/", TaskUserArchiveView.as_view(), name="user-archive-tasks"),
+    path("task/<int:pk>/", task_in_archive , name="task-in-archive"),
+    path("tasks-review-list/", TaskReviewListView.as_view(), name="tasks-review-list")
+
+]
