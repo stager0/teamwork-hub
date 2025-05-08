14ea061e38fa0d19f423e72097c3f73899343da6
Andrii Shtaher <andriishtaher@gmail.com>
2025-05-08 15:29:55 +0200

    fix: settings.py

--- teamwork_hub/settings.py
+++ teamwork_hub/settings.py
@@ -26,14 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.dotenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
-
 
 
 # Application definition
